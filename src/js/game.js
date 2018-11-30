ac45cd7bc1e173f5fac783658ff7b5d3a43464d7
Jonas Wirth <wirth@matoma.de>
2018-11-30 18:08:35 +0100

    Scaffolded folder structure. First steps in Vue.js

---
+++ src/js/game.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#game',
+    data: {
+        message: 'Hello Vue',
+        columns:[
+            1,2,3
+        ],
+        rows:[
+            1,2,3,4
+        ]
+    }
+})
