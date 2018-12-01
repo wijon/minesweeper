c21ebca74b3f03c48aaaf572e695b204b826b20d
wijon <44641733+wijon@users.noreply.github.com>
2018-12-01 17:09:15 +0100

    Started implementing js model.

---
+++ src/js/classes.js
@@ -0,0 +1,80 @@
+"use strict";
+
+class Field {
+    constructor(x, y) {
+        this.position = { x: x, y: y }
+        this.hasMine = false;
+        this.hasFlag = false;
+        this.isExplored = false;
+        this.nearByMineCounter = 0;
+    }
+}
+
+class Board {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+        this.fields = []
+    }
+
+    static create(height, width) {
+        if (height < 1 || width < 1) {
+            throw "The board must be at least 1x1";
+        }
+
+        let board = new Board(height, width);
+        board.addFields();
+        board.placeMines(2);
+
+        return board;
+    }
+
+    addFields() {
+        let fields = [];
+
+        for (let i = 1; i <= this.width; i++) {
+            for (let j = 1; j <= this.height; j++) {
+                fields.push(new Field(i, j));
+            }
+        }
+
+        this.fields = fields;
+    }
+
+    placeMines(mineCount) {
+        if (typeof this.fields === 'undefined' || this.fields.length < 1) {
+            throw "Board does not have any fields defined."
+        }
+
+        if (mineCount >= this.fields.length) {
+            throw "At least one non mine field is required";
+        }
+
+        // Take random fields as mine fields
+        let mineFields = this.fields.slice(0).sort(function (a, b) {
+            if (Math.random() >= 0.5) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }).slice(0, mineCount);
+
+        // Set the has mineFlag
+        mineFields.forEach(function (field) { field.hasMine = true });
+
+        this.fields.forEach(calcNearBy)
+    }
+
+    calcNearBy(field) {
+        if (field.hasMine) {
+            // No need to calculate near by count for fields with mines
+            return;
+        }
+
+        field.nearByMineCounter = getNeighbours(field);
+    }
+
+    getNeighbours(field) {
+        
+    }
+}
--- src/js/game.js
+++ src/js/game.js
@@ -1,26 +1,31 @@
+"use strict";
+
 Vue.component('field-square', {
     props: ['square'],
     template: `<div class="bg-secondary field-square" @mouseup.right="$emit('flag')" @mouseup.left="$emit('explore')" @contextmenu.prevent>{{square}}</div>`
 })
 
-var points = {};
-points[[1,1]] = "1,1";
-points[[1,2]] = "1,2";
-points[[1,3]] = "1,3";
 
-points[[2,1]] = "2,1";
-points[[2,2]] = "2,2";
-points[[2,3]] = "2,3";
+console.log(Board.create(3,4));
+
+var points = {};
+points[[1, 1]] = "1,1";
+points[[1, 2]] = "1,2";
+points[[1, 3]] = "1,3";
+
+points[[2, 1]] = "2,1";
+points[[2, 2]] = "2,2";
+points[[2, 3]] = "2,3";
 
 var app = new Vue({
     el: '#game',
     data: {
         message: 'Hello Vue',
         columns: [
-            1,2
+            1, 2
         ],
         rows: [
-            1,2,3
+            1, 2, 3
         ],
         points
     },
