c73001c0210f966143e57e1b2eb60611f711bd29
wijon <44641733+wijon@users.noreply.github.com>
2018-12-02 18:25:42 +0100

    Finished implementation of js model. Game is now playable.

--- src/js/classes.js
+++ src/js/classes.js
@@ -7,6 +7,7 @@ class Field {
         this.hasFlag = false;
         this.isExplored = false;
         this.nearByMineCounter = 0;
+        this.isGameOverClick = false;
     }
 }
 
@@ -17,14 +18,14 @@ class Board {
         this.fields = []
     }
 
-    static create(height, width) {
+    static create(height, width, mineCount) {
         if (height < 1 || width < 1) {
             throw "The board must be at least 1x1";
         }
 
         let board = new Board(height, width);
         board.addFields();
-        board.placeMines(2);
+        board.placeMines(mineCount);
 
         return board;
     }
@@ -62,6 +63,7 @@ class Board {
         // Set the has mineFlag
         mineFields.forEach(function (field) { field.hasMine = true });
 
+        // Calculate nearby mines for every field
         // Use arrow function to preserve 'this' in called method 
         this.fields.forEach((field) => this.calcNearBy(field));
     }
@@ -72,10 +74,156 @@ class Board {
             return;
         }
 
-        field.nearByMineCounter = this.getNeighbours(field);
+        field.nearByMineCounter = this.getNeighbours(field).filter(f => f.hasMine).length;
     }
 
     getNeighbours(field) {
-        
+        const position = field.position;
+        let neighbours = [];
+
+        for (let i = position.x - 1; i <= position.x + 1 && i <= this.width; i++) {
+            for (let j = position.y - 1; j <= position.y + 1 && j <= this.height; j++) {
+                // Handle border crossing
+                var neighbourX = i < 1 ? 1 : i;
+                var neighbourY = j < 1 ? 1 : j;
+
+                const neighbourField = this.fields.find(f => f.position.x === neighbourX && f.position.y === neighbourY);
+
+                if ((neighbourX == position.x && neighbourY == position.y) || neighbours.includes(neighbourField)) {
+                    // Ignore the initial point and already listed points
+                    continue;
+                }
+
+                neighbours.push(neighbourField);
+            }
+        }
+
+        return neighbours;
+    }
+
+    explore(field) {
+        if (!field) {
+            // Field is not defined
+            return false;
+        }
+
+        if (field.hasMine) {
+            // Field has mine, Game Over
+            field.isExplored = true;
+            return false;
+        }
+
+        this.starExploration(field.position, true);
+
+        return true;
+    }
+
+    starExploration(position) {
+        let currentField = this.fields.find(f => f.position.x === position.x && f.position.y === position.y);
+
+        if (!currentField) {
+            return;
+        }
+
+        if (currentField.isExplored) {
+            // Already explored, nothing to do
+            return;
+        }
+
+        if (currentField.hasFlag) {
+            // If the field is flagged, do nothing
+            return;
+        }
+
+        currentField.isExplored = true;
+
+        if (currentField.hasMine) {
+            // If field has mine, do nothing
+            return;
+        }
+
+        if (currentField.nearByMineCounter > 0) {
+            // If the field has mines nearby, return
+            return;
+        }
+
+        this.starExploration({ x: currentField.position.x - 1, y: currentField.position.y + 1 });
+        this.starExploration({ x: currentField.position.x - 1, y: currentField.position.y + 0 });
+        this.starExploration({ x: currentField.position.x - 1, y: currentField.position.y - 1 });
+        this.starExploration({ x: currentField.position.x + 0, y: currentField.position.y - 1 });
+        this.starExploration({ x: currentField.position.x + 1, y: currentField.position.y - 1 });
+        this.starExploration({ x: currentField.position.x + 1, y: currentField.position.y + 0 });
+        this.starExploration({ x: currentField.position.x + 1, y: currentField.position.y + 1 });
+        this.starExploration({ x: currentField.position.x + 0, y: currentField.position.y + 1 });
+    }
+
+    getFieldByPosition(position) {
+        return this.fields.find(f => f.position.x === position.x && f.position.y === position.y);
+    }
+
+    getNumberOfFlags() {
+        return this.fields.filter(f => f.hasFlag).length;
+    }
+
+    exploreAll() {
+        this.fields.forEach(f => f.isExplored = true);
+    }
+}
+
+class Game {
+    constructor() {
+        this.isGameOver = false;
+        this.passedSeconds = 0;
+
+        this.timerInterval = setInterval(() => {
+            this.passedSeconds++;
+        }, 1000);
+    }
+
+    static create(boardHeight, boardWidth, mineCount) {
+        let game = new Game();
+        game.board = Board.create(boardHeight, boardWidth, mineCount);
+        game.mineCount = mineCount;
+
+        return game;
+    }
+
+    exploreField(field) {
+        if (!this.isGameOver) {
+            let isGameOver = !this.board.explore(field);
+
+            if (isGameOver) {
+                field.isGameOverClick = true;
+                this.executeGameOver();
+            } else {
+                this.checkWinCondition();
+            }
+        }
+
+        return this.isGameOver;
+    }
+
+    flagField(field) {
+        if (!this.isGameOver && !field.isExplored) {
+            field.hasFlag = !field.hasFlag;
+        }
+    }
+
+    getRemainingMineCount() {
+        return this.mineCount - this.board.getNumberOfFlags();
+    }
+
+    executeGameOver() {
+        this.isGameOver = true;
+        clearInterval(this.timerInterval);
+        this.board.exploreAll();
+    }
+
+    checkWinCondition() {
+        if (!this.board.fields.some(f => !f.hasMine && !f.isExplored)) {
+            // Game Won
+            console.log("winner");
+            clearInterval(this.timerInterval);
+        }
     }
 }
--- src/js/game.js
+++ src/js/game.js
@@ -1,40 +1,79 @@
 "use strict";
 
-Vue.component('field-square', {
-    props: ['square'],
-    template: `<div class="bg-secondary field-square" @mouseup.right="$emit('flag')" @mouseup.left="$emit('explore')" @contextmenu.prevent>{{square}}</div>`
-})
+Vue.component('field-element', {
+    props: ['field'],
+    template: `
+    <div class="field" v-bind:class="getFieldBgColor()" @mouseup.right="$emit('flag', field)" @mouseup.left="$emit('explore', field)"@contextmenu.prevent>
+        <div v-if="!field.isExplored">
+            <i class="fa fa-flag" v-if="field.hasFlag"></i>
+        </div>
+        <div v-else>
+            <i class="fa fa-asterisk" v-if="field.hasMine"></i>
+            <span v-else-if="!field.hasFlag && field.nearByMineCounter > 0">{{field.nearByMineCounter}}</span>
+        </div>
+    </div>`,
+    methods: {
+        getFieldBgColor() {
+            return {
+                'bg-light': this.field.isExplored && !this.field.hasMine,
+                'bg-secondary': !this.field.isExplored,
+                'bg-danger': this.field.isExplored && this.field.hasMine && this.field.isGameOverClick
+            };
+        }
+    }
+});
 
+Vue.component('timer', {
+    props: ['passed-seconds'],
+    template: `
+    <div>
+        {{hours | two_digits}}:{{minutes | two_digits}}:{{seconds | two_digits}}
+    </div>`,
+    computed: {
+        seconds() {
+            return this.passedSeconds % 60;
+        },
+        minutes() {
+            return Math.trunc(this.passedSeconds / 60) % 60;
+        },
+        hours() {
+            return Math.trunc(this.passedSeconds / 60 / 60) % 24;
+        }
+    }
+});
 
-console.log(Board.create(3,4));
+Vue.filter('two_digits', function (value) {
+    if (value.toString().length <= 1) {
+        return "0" + value.toString();
+    }
+    return value.toString();
+});
 
-var points = {};
-points[[1, 1]] = "1,1";
-points[[1, 2]] = "1,2";
-points[[1, 3]] = "1,3";
-
-points[[2, 1]] = "2,1";
-points[[2, 2]] = "2,2";
-points[[2, 3]] = "2,3";
+function createGame() {
+    return Game.create(14, 18, 10);
+}
 
 var app = new Vue({
     el: '#game',
     data: {
-        message: 'Hello Vue',
-        columns: [
-            1, 2
-        ],
-        rows: [
-            1, 2, 3
-        ],
-        points
+        game: createGame()
     },
     methods: {
-        flag() {
-            console.log("flag");
+        flag(field) {
+            this.game.flagField(field);
         },
-        explore() {
-            console.log("explore")
+        explore(field) {
+            this.game.exploreField(field);
+        },
+        newGame(event) {
+            this.game = createGame();
+            // Remove focus from button
+            event.target.blur();
+        }
+    },
+    computed: {
+        remainingMineCount: function () {
+            return this.game.getRemainingMineCount();
         }
     }
-})
+});
